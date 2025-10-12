b1c8f808cad83692da9fdfd52565ff8663dca56b
jutt16 <shahidfaqeer02@gmail.com>
2025-10-13 00:42:51 +0500

    v1

--- app/Http/Controllers/Api/PartnerProfileController.php
+++ app/Http/Controllers/Api/PartnerProfileController.php
@@ -58,7 +58,7 @@ class PartnerProfileController extends Controller
                 'police_verification' => 'nullable|file|mimes:jpg,jpeg,png,pdf',
                 'covid_vaccination_certificate' => 'nullable|file|mimes:jpg,jpeg,png,pdf',
                 'selfie_with_costume' => 'nullable|file|mimes:jpg,jpeg,png',
-                'intro_video' => 'nullable|file|mimes:mp4,mov,avi',
+                'intro_video' => 'nullable|file|mimetypes:video/*',
                 'service_ids' => 'nullable|array', // services partner offers
                 'service_ids.*' => 'integer|exists:services,id',
             ]);
