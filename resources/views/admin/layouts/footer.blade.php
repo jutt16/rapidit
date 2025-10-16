4c2ea0394126f26f16159032922655ae15bc7be0
jutt16 <shahidfaqeer02@gmail.com>
2025-10-17 01:18:06 +0500

    v1

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2025 <a href="#">The ConsultX</a>.</strong>
+    <strong>Copyright &copy; 2025 <a href="https://rapidit.in">Rapidit</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
         <b>Version</b> 3.2.0
